31076e498de4488beb878fc113864d641fbde7f8
weihe <weihe@cainfoharbor.com>
2018-05-29 20:24:25 +0800

    error

--- dist/js/error-0b94e1a8.js
+++ dist/js/error-0b94e1a8.js
@@ -217,18 +217,40 @@ module.exports = Common;
 /***/ 20:
 /***/ (function(module, exports, __webpack_require__) {
 
-var css = __webpack_require__(58);
+var css = __webpack_require__(21);
 var com = __webpack_require__(0);
 
 var deviceId = com.parseQuery('deviceId');
 var slotIndex = com.parseQuery('slotIndex');
 var errorCode = com.parseQuery('errorCode');
 
+var map = {
+    1001: '设备故障'
+}
+
+$(document).ready(function () {
+    $('.error-text').text(map[errorCode]);
+    $('.error-deviceId').text(deviceId);
+    $('.error-slotIndex').text(slotIndex);
+    com.getWxConfig();  
+    wx.ready(function () {
+        $('.retry-btn').on('click', function () {
+            wx.scanQRCode({
+                needResult: 0, // 默认为0，扫描结果由微信处理，1则直接返回扫描结果，
+                scanType: ["qrCode","barCode"], // 可以指定扫二维码还是一维码，默认二者都有
+                success: function (res) {
+                    var result = res.resultStr; // 当needResult 为 1 时，扫码返回的结果
+                }
+            });
+        });
+    });
+})
+
 
 
 /***/ }),
 
-/***/ 58:
+/***/ 21:
 /***/ (function(module, exports) {
 
 // removed by extract-text-webpack-plugin
